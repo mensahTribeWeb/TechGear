9150e350c9e49d08f8647b70c0a6291c82f02102
Nick Mensah(Nokore.dev) <ndm.acct85@gmail.com>
2023-10-06 09:56:02 -0500

    Add field for mission statement and about paragraph, Add Constructors, getters, setters, toString

--- src/main/java/com/example/demo/domain/AboutInfo.java
+++ src/main/java/com/example/demo/domain/AboutInfo.java
@@ -1,4 +1,43 @@
 package com.example.demo.domain;
 
 public class AboutInfo {
+    private String missionStatement;
+    private String aboutParagraph;
+
+    //constructors
+    public AboutInfo() {
+    }
+
+    public AboutInfo(String missionStatement, String aboutParagraph) {
+        this.missionStatement = missionStatement;
+        this.aboutParagraph = aboutParagraph;
+    }
+
+    //getters
+
+    public String getMissionStatement() {
+        return missionStatement;
+    }
+
+    public String getAboutParagraph() {
+        return aboutParagraph;
+    }
+
+    //setters
+
+    public void setMissionStatement(String missionStatement) {
+        this.missionStatement = missionStatement;
+    }
+
+    public void setAboutParagraph(String aboutParagraph) {
+        this.aboutParagraph = aboutParagraph;
+    }
+
+    @Override
+    public String toString() {
+        return "AboutInfo{" +
+                "missionStatement='" + missionStatement + '\'' +
+                ", aboutParagraph='" + aboutParagraph + '\'' +
+                '}';
+    }
 }
