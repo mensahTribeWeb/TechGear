db8b15e4643bb835c16bd8ccdf3ea3e8df265a4b
Nick Mensah(Nokore.dev) <ndm.acct85@gmail.com>
2023-10-06 08:39:52 -0500

    Add: Controller notion, Service info to obtain about data to pass to the view, setup mapping for http request

--- src/main/java/com/example/demo/controllers/AboutController.java
+++ src/main/java/com/example/demo/controllers/AboutController.java
@@ -1,2 +1,24 @@
-package com.example.demo.controllers;public class AboutController {
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AboutController {
+    private final AboutService aboutService;
+
+    @Autowired
+    public AboutController(AboutService aboutService) {
+        this.aboutService = aboutService;
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        AboutInfo aboutInfo = aboutService.getAboutInfo();
+        model.addAttibute("aboutInfo", aboutInfo);
+        return "about";
+    }
+
 }
