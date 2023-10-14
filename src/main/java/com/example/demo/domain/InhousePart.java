b60c205a135b2c88367cd0a4dde5aad309ee0651
Nick Mensah(Nokore.dev) <ndm.acct85@gmail.com>
2023-10-14 11:50:46 -0500

    Add: javadoc and specified constructor based on starter parts in bootstrap

--- src/main/java/com/example/demo/domain/InhousePart.java
+++ src/main/java/com/example/demo/domain/InhousePart.java
@@ -4,10 +4,8 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 /**
- *
- *
- *
- *
+ * Represents an in-house part in the system, extending the base Part class.
+ * In-house parts are parts that are produced or maintained internally by the company.
  */
 @Entity
 @DiscriminatorValue("1")
@@ -17,11 +15,31 @@ public class InhousePart extends Part{
     public InhousePart() {
     }
 
+    public InhousePart(long id, String name, double price, int inv, int partId) {
+        super(id, name, price, inv);
+        this.partId = partId;
+    }
+    /**
+     * Constructor for creating an in-house part with specified attributes.
+     *
+     * @param name  The name of the in-house part.
+     * @param price The price of the in-house part.
+     * @param inv   The inventory quantity of the in-house part.
+     */
+    public InhousePart(String name, double price, int inv) {
+        super(name, price, inv);
+    }
+
     public int getPartId() {
         return partId;
     }
 
     public void setPartId(int partId) {
         this.partId = partId;
+    }
+
+    @Override
+    public String toString() {
+        return "Part [id=" + id + ", name=" + name + ", price=" + price + ", inv=" + inv + "]";
     }
 }
