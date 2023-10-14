8ee03bc779258d05b774229adcbd6b996aab7a5c
Nick Mensah(Nokore.dev) <ndm.acct85@gmail.com>
2023-10-14 11:28:03 -0500

    FixMe: working on the postMapping for AddInHouseController

--- src/main/java/com/example/demo/controllers/AddInhousePartController.java
+++ src/main/java/com/example/demo/controllers/AddInhousePartController.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import com.example.demo.domain.InhousePart;
 import com.example.demo.service.InhousePartService;
+import com.example.demo.service.InhousePartServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,7 +17,7 @@ import javax.validation.Valid;
  * Controller class for handling operations related to adding and saving Inhouse Parts.
  */
 @Controller
-public class AddInhousePartController{
+public class AddInhousePartController {
 
     @Autowired
     private InhousePartService inhousePartService;
@@ -28,34 +29,60 @@ public class AddInhousePartController{
      * @return The name of the HTML template for the Inhouse Part form.
      */
     @GetMapping("/showFormAddInPart")
-    public String showFormAddInhousePart(Model theModel){
-        InhousePart inhousepart= new InhousePart();
-        theModel.addAttribute("inhousepart",inhousepart);
+    public String showFormAddInhousePart(Model theModel) {
+        InhousePart inhousepart = new InhousePart();
+        theModel.addAttribute("inhousepart", inhousepart);
         return "InhousePartForm";
     }
+
     /**
      * Handles the submission of the Inhouse Part form.
      *
-     * @param inhousePart    The Inhouse Part to be added, automatically populated from the form.
-     * @param bindingResult  The binding result for validation.
+     * @param inhousePart   The Inhouse Part to be added, automatically populated from the form.
+     * @param bindingResult The binding result for validation.
      * @return The appropriate view based on validation results, "InhousePartForm" if there are errors,
-     *         "confirmationaddpart" if the form is successfully submitted and the Inhouse Part is saved.
+     * "confirmationaddpart" if the form is successfully submitted and the Inhouse Part is saved.
      */
     @PostMapping("/showFormAddInPart")
     public String submitForm(@Valid @ModelAttribute("inhousepart") InhousePart inhousePart, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "InhousePartForm";
         } else {
-            // You don't need to manually create the part or get it from the context
             inhousePartService.save(inhousePart);
             return "confirmationaddpart";
         }
     }
+
+    @PostMapping("/addInhousePart")
+    public String addInhousePart(@Valid @ModelAttribute("inhousepart") InhousePart inhousePart, BindingResult bindingResult, Model theModel) {
+        if (bindingResult.hasErrors()) {
+            return "InhousePartForm";
+        }
+
+        // Check if minInv is less than or equal to maxInv
+        if (inhousePart.getMinInv() > inhousePart.getMaxInv()) {
+            // Display an error message
+            theModel.addAttribute("errorMessage", "Error: Minimum inventory cannot be greater than maximum inventory.");
+            return "error-page"; // Redirect to an error page
+        }
+
+        // Check if inventory is within the valid range
+        if (inhousePart.isInvValid()) {
+            inhousePartService.save(inhousePart);
+            return "success-page"; // Redirect to a success page
+        } else {
+            // Display an error message
+            theModel.addAttribute("errorMessage", "Error: Inventory couldn't be larger than the maximum or smaller than the minimum.");
+            return "error-page"; // Redirect to an error page
+        }
+    }
+
+
     /**
      * Handles saving an Inhouse Part, performing additional validation.
      *
-     * @param inhousePart    The Inhouse Part to be saved.
-     * @param bindingResult  The binding result for validation.
+     * @param inhousePart   The Inhouse Part to be saved.
+     * @param bindingResult The binding result for validation.
      * @return The appropriate view based on validation results.
      */
     @PostMapping("/saveInhousePart")
