c7b8f60d6414683d4498dc6a5d3fcc3c432d3a6b
Nick Mensah(Nokore.dev) <ndm.acct85@gmail.com>
2023-10-29 20:04:23 -0500

    Refactor: iterate through inhousePart create an constructor if present

--- src/main/java/com/example/demo/validators/EnufPartsValidator.java
+++ src/main/java/com/example/demo/validators/EnufPartsValidator.java
@@ -1,5 +1,6 @@
 package com.example.demo.validators;
 
+import com.example.demo.domain.InhousePart;
 import com.example.demo.domain.Part;
 import com.example.demo.domain.Product;
 import com.example.demo.service.ProductService;
@@ -43,6 +44,9 @@ public class EnufPartsValidator implements ConstraintValidator<ValidEnufParts, P
         if (product.getId() != 0) {
             Product myProduct = repo.findById((int) product.getId());
             for (Part p : myProduct.getParts()) {
+                if(p instanceof InhousePart){
+                    InhousePart inhousePart = new InhousePart(p.getId(), p.getName(), p.getPrice(), p.getInv(), p.getMinInv(), p.getMaxInv(), p.getProducts() ,((InhousePart) p).getPartId());
+                }
                 if (p.getInv()<(product.getInv()-myProduct.getInv()))return false;
             }
             return true;
